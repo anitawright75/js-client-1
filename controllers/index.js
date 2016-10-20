405684f64283d5e1823805dd44660552e3b83d50
code-for-coffee <jtraverjr@gmail.com>
2016-10-20 16:18:40 -0500

    simple client app

---
+++ controllers/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var ctrl = express.Router();
+
+/* GET home page. */
+ctrl.get('/', function(req, res, next) {
+  res.render('index', { title: 'NSATRACKER.COM' });
+});
+
+/* GET our users */
+ctrl.get('/view', function(req, res, next) {
+  res.render('users', { title: "AJAX" });
+});
+
+module.exports = ctrl;
